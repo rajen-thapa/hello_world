be1bcb30ed8c521d066cb0ae00550f1901f9f5e8
rajen-thapa <63527798+rajen-thapa@users.noreply.github.com>
2021-07-22 18:53:55 +0545

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 import pymongo
 from pymongo import MongoClient
-client = pymongo.MongoClient('mongodb://192.168.136.130:27017/Production')
+client = pymongo.MongoClient('mongodb://192.168.136.130:27017/Production',
+                            username='rnwuser',
+                            password='Irg@370ahmz')
 mydb = client["Production"]
 information = mydb["jiten"]
 record = [{"name": "Krishna", "age": 27},{"name": "Shanti", "age": 50}]
